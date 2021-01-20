e007a1bebc0695b825d446d5a759ba04ef648c2e
gdiprisco <g.diprisco3@studenti.unisa.it>
2021-01-20 15:20:22 +0100

    composing data for mosaic, no creation

--- evaluate/compose_mosaics.py
+++ evaluate/compose_mosaics.py
@@ -13,7 +13,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 sys.path.append("../dataset")
 from dataset_utils import LABELS
-
+from cv2 import cv2
 ############## PARAMS ################
 ckp_number = sys.argv[1]
 ######################################
@@ -30,24 +30,95 @@ versions = ["verA", "verB", "verC"]
 inpath = "results/thesis_results_{ckp}/results_{dataset}_{partition}_of_checkpoint_{ckp}_from_net{backbone}_versionver[ABC]_*"
 outpath = "results/thesis_stats_{ckp}/{ckp}_mosaic_{backbone}_{dataset}_{partition}.png"
 
+def create_mosaic(image_paths, predictions, originals, size):
+    # rows, columns = size
+    # imagex1, imagey1 = 224,224
+    # blbx0, blby0 = imagex1+1, imagey1+1
+    # blbx1, blby1 = blbx0+200, imagey1
+    # grbx0, grby0 =
+    # grbx1, grby1 = 
+    # for r in rows:
+    #     for c in columns:
+    #         current_index = r*c
+    #         image_path = image_paths[current_index]
+    #         image = cv2.imread(image_path)
+    #         assert image is not None, "Error loading image {}".format(image_path)
+
+    #         # # TODO add roi selection support
+    #         # if not (image_roi is None or avoid_roi):
+    #         #   image = cut(image, image_roi)
+
+    #         # values    
+            
+
+
+    #         image = cv2.resize(image, (image_dx, image_dy))
+    #         cv2.rectangle(image,(224,0),(200,224),(0,0,0),cv2.FILLED) # predictions box
+    #         cv2.rectangle(image,(424,0),(200,224),(0,255,0),cv2.FILLED) # annotations box
+    #         cv2.putText(image,str(image_value),(10,25),cv2.FONT_HERSHEY_SIMPLEX,1.0,(255,255,255),2)
+    #         cv2.rectangle(image,(91,0),(180,35),(0,0,255),cv2.FILLED)
+    #         cv2.putText(image,str(image_original_value),(100,25),cv2.FONT_HERSHEY_SIMPLEX,1.0,(255,255,255),2)
+
+
+def convlab(label, task):
+    return np.argmax(label) if task != "age" else label
+
+def get_indices(data_dict, image_paths):
+    return [data_dict['image_paths'].index(path) for path in image_paths]
+
 def get_path(data_dict, index):
     return data_dict['image_paths'][index]
 
-def get_prediction(data_dict, task, index):
-    return data_dict['predictions'][task][index]
+def get_predictions(data_dict, task, indices):
+    return [convlab(data_dict['predictions'][task][index], task) for index in indices]
 
-def get_original(data_dict, task, index):
-    return data_dict['original_labels'][task][index]
+def get_originals(data_dict, task, indices):
+    return [convlab(data_dict['original_labels'][task][index], task) for index in indices]
+    
 
 def mosaic(verA, verB, verC, task_list, outpath, size=(4,5)):
-    #####
-    # rows, columns = size
-    # verA_zip
-    # indices_A = np.array(random.sample(list(enumerate(verA_zip)), k=rows*columns))
+    rows, columns = size
+    indices = dict()
+    predictions = defaultdict(dict)
+    originals = dict()
+    random_A = np.array(random.sample(list(enumerate(verA['image_paths'])), k=rows*columns))
+    image_paths = list()
+    indices["verA"] = list()
 
-    # predicted = result_dict['predictions'][task]
-    # original = result_dict['original_labels'][task]
-    return ""
+    for item in random_A:
+        indices["verA"].append(int(item[0]))
+        image_paths.append(item[1])
+    indices["verB"] = get_indices(verB, image_paths)
+    indices["verC"] = get_indices(verC, image_paths)
+
+    '''
+    image_paths = [path1, path2, path3, pathN]
+
+    predictions = {
+        taskX : {
+            verY : [value1, value2, value3, valueN]
+        }
+    }
+
+    After original labels check and compression:
+    originals = {
+        taskX : [value1, value2, value3, valueN]
+    }
+
+    '''
+    
+    for data_name, data_dict in zip(["verA", "verB", "verC"], [verA, verB, verC]):
+        for task in task_list:
+            predictions[task][data_name] = get_predictions(data_dict, task, indices[data_name])
+            tmp_originals = get_originals(data_dict, task, indices[data_name])
+            if task not in originals:
+                originals[task] = tmp_originals
+            elif originals[task] != tmp_originals:
+                raise Exception("Original labels unmatch between {} and its previous version".format(data_name))
+    
+    # mosaic = create_mosaic(image_paths, predictions, originals, size)
+    # cv2.imwrite(outpath, mosaic)
+    return outpath
 
 # def create_mosaic(reference, out_path, size=(4,5), avoid_roi=False, images_root=None):
 #     rows, columns = size    
