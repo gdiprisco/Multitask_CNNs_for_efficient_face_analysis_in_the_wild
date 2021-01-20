5c45ea7dc8ed95f09175ddf02c7c05d23066466b
gdiprisco <g.diprisco3@studenti.unisa.it>
2021-01-20 02:15:07 +0100

    divide mosaics skeleton

--- evaluate/evaluate_results.py
+++ evaluate/evaluate_results.py
@@ -70,54 +70,6 @@ def save_plot_confusion_matrix(result_dict, task, filepath):
     plt.savefig(filepath)
     return array
 
-def mosaic(data, outpath):
-    # predicted = result_dict['predictions'][task]
-    # original = result_dict['original_labels'][task]
-    return ""
-
-# def create_mosaic(reference, out_path, size=(4,5), avoid_roi=False, images_root=None):
-#     rows, columns = size    
-#     mosaic = None
-#     extra_dim = np.array(reference).shape[1]
-#     mosaic_items = np.array(random.choices(reference, k=rows*columns))
-#     mosaic_items = np.reshape(mosaic_items, (rows, columns, extra_dim))
-#     mse = None
-#     mae = None
-
-#     for i in range(rows):
-#         mosaic_row = None
-#         for j in range(columns):
-#             mosaic_item = mosaic_items[i][j]
-
-#             image_path = mosaic_item[0] if images_root is None else os.path.join(images_root, mosaic_item[0])
-#             image_value = np.round(np.float(mosaic_item[1]), decimals=1)
-#             image_original_value = np.round(np.float(mosaic_item[2]), decimals=1) if mosaic_item[2] is not None else None
-#             image_roi = mosaic_item[3] if mosaic_item[3] is not None else None
-            
-#             image = cv2.imread(image_path)
-#             assert image is not None, "Error loading image {}".format(image_path)
-
-#             if not (image_roi is None or avoid_roi):
-#                 image = cut(image, image_roi)
-
-#             image = cv2.resize(image, (224, 224))
-#             cv2.rectangle(image,(0,0),(90,35),(0,0,0),cv2.FILLED)
-#             cv2.putText(image,str(image_value),(10,25),cv2.FONT_HERSHEY_SIMPLEX,1.0,(255,255,255),2)
-
-#             if image_original_value is not None:
-#                 cv2.rectangle(image,(91,0),(180,35),(0,0,255),cv2.FILLED)
-#                 cv2.putText(image,str(image_original_value),(100,25),cv2.FONT_HERSHEY_SIMPLEX,1.0,(255,255,255),2)
-#                 square_error = (image_original_value-image_value)**2
-#                 mse = square_error if mse is None else mse+square_error
-#                 absolute_error = abs(image_original_value-image_value)
-#                 mae = absolute_error if mae is None else mae+absolute_error
-
-#             mosaic_row = image if mosaic_row is None else np.concatenate((mosaic_row, image), axis=1)
-#         mosaic = mosaic_row if mosaic is None else np.concatenate((mosaic,mosaic_row),axis=0)
-#     cv2.imwrite(out_path, mosaic)
-#     mse = np.round(mse/(rows*columns), decimals=3) if mse is not None else None
-#     mae = np.round(mae/(rows*columns), decimals=3) if mae is not None else None
-#     return mse, mae
 
 def statistic(v1, v2, metric):
     if metric == "tstat":
@@ -172,11 +124,6 @@ if __name__ == "__main__":
     os.makedirs(cm_plot_subfolder, exist_ok=True)
     print("All confusion matrices in", cm_plot_subfolder, "will be overwritten...")
 
-    mosaic_subfolder = suboutpath.format(ckp=ckp_number, aim="mosaics")
-    os.makedirs(mosaic_subfolder, exist_ok=True)
-    print("All mosaics in", mosaic_subfolder, "will be overwritten...")
-
-
     for backbone in backbones:
         for dataset in datasets.keys():
             print("\nOpening", dataset, backbone, "results...")
@@ -187,26 +134,8 @@ if __name__ == "__main__":
                 ckp=ckp_number
             )))
             verA = pickle.load(open(verA_path, "rb"))
-            # mosaic(verA, outpath.format(
-            #     ckp=ckp_number,
-            #     dataset=dataset,
-            #     partition=partition,
-            #     backbone=backbone,
-            #     version="verA"))
             verB = pickle.load(open(verB_path, "rb"))
-            # mosaic(verB, outpath.format(
-            #     ckp=ckp_number,
-            #     dataset=dataset,
-            #     partition=partition,
-            #     backbone=backbone,
-            #     version="verB"))
             verC = pickle.load(open(verC_path, "rb"))
-            # mosaic(verC, outpath.format(
-            #     ckp=ckp_number,
-            #     dataset=dataset,
-            #     partition=partition,
-            #     backbone=backbone,
-            #     version="verC"))
             for task in datasets[dataset]:
                 title = "{bb}_{task}_{dataset}_{partition}".format(
                     bb=backbone,
